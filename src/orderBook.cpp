061aa9b6f4e867199f1b98d5a36c552885bbfc71
wailo <wailo_2@hotmail.com>
2019-08-05 23:19:20 +0200

    Fixed return type

--- src/orderBook.cpp
+++ src/orderBook.cpp
@@ -8,18 +8,18 @@ bool orderBook::addOrder( order p_order )
   // Orders are stored in heap    
   if ( p_order.side() == orderSide::BUY )
   {
-    m_buyOrders.push_back(p_order); 
+    m_buyOrders.emplace_back(std::move(p_order));
     std::push_heap(m_buyOrders.begin(), m_buyOrders.end(), std::less<order>());
   }
   else if ( p_order.side() == orderSide::SELL )
   {
-    m_sellOrders.push_back(p_order); 
+    m_sellOrders.emplace_back(std::move(p_order));
     std::push_heap(m_sellOrders.begin(), m_sellOrders.end(), std::greater<order>());
   }
   else
   {
     std::cout << "invalid order type" << std::endl;
-    return 0;
+    return false;
   }
     
   return true;
